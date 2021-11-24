fc03697f1aaff20ad1bbeb469a66ee08f95d884e
Dom3005 <30704218+Dom3005@users.noreply.github.com>
2021-11-24 18:07:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,129 @@
+// const fs = require('fs');
+
+let fieldsMol = 
+    ["Holzweiler", "Wie ist die Stimmung?", "Buch auf Seite X", "Stoff aus der EF", "Habt ihr noch nicht gelernt",
+    "Kommt erst in der Q2", "Das ist eigentlich Unterstufenstoff", "Das ist eigentlich Mittelstufenstoff", "Frage oder Anwort?", "Kommt erkaeltet in den Unterricht",
+    "Macht mal bitte Aufgabe X auf Seite Y als Hausaufgabe", "Die Hausaufgaben macht ihr fuer euch, nicht fuer mich", "Vergisst Hausaufgaben aufgegeben zu haben", "Das ist Klausurrelevant", "Das ist nicht Klausurrelevant",
+    "Buch aufschlagen", "Ihr macht jetzt bitte Aufgabe X fuer mich", "Fenster auf", "Redet ueber Fallzahlen", "Verstanden?",
+    "Mittelmaessige Skizze", "Wer hat hier Physik?", "Benutzt Abituraufgaben", "Erscheint zu spaet", "Da haette ich mehr erwartet",
+    "Redet ueber Geogebra", "Ich habe ja einen anderen Taschenrechner", "Das geht auch schneller", "Von Dennis weiss ich schon dass er das kann", "Ich hab euch was auf Moodle hochgeladen",
+    "Das ist freiwillig", "Leuteee, das solltet ihr eigentlich koennen", "Da muss ich gleich mal in die Physiksammlung", "Wir liegen hinter dem Zeitplan", "Eigentlich habe ich ja was anderes geplant",
+    "Regt sich auf", "1-Hand Clap", "Kurzes Handzeichen", "Jungs & Maedels", "Maedels",
+    "Jungs", "habe ich schonmal gesagt", "naechste Woche", "wer schon fertig ist...", "wer hat einen funktionsfähigen Tachenrechner?",
+    "das muesste man pruefen", "Sekunde"];
+
+let fieldsE = 
+    ["In English please", "Spricht Namen falsch aus", "Fake Accent", "Von Fr. Barnard verbessert", "Fr. Barnard beschwert sich",
+    "Unlustiger Spruch", "Steht vorne ohne Maske", "Anyone else?", "Kommt zu spaet", "Unterbricht andere",
+    "Versteht Frage falsch", "Diskutiert mit irgendwem", "Sieht ihre Fehler nicht ein", "Kennt Namen nicht", "Schaut auf Sitzplan nach Namen"];
+
+let fields =
+    ["Ääää.. öööö.. ääh", "Fenster auf", "Atlanten", "Erwähnt seine Reisen", "Oh.. da war ich schonmal",
+    '"verspricht" Bilder zu zeigen', "letzte Reihe", "Unterbricht andere", "Überprüft Anwesenheit", "in a minute",
+    "kommt noch/erst in der Q2", "Mittelstufenstoff", "AHA!", "gesteht seine Fehler nicht ein", "diskutiert mit Tobias",
+    "mittelmäßige Skizze", "Schreibt Vokabeln an", "Es sind immer die selben!", "WOTT?", "Fängt Deutsch an, endet Englisch",
+    "YESSS!", "vielleicht schonmal gehört", "Anekdoten", "wie sagt man..", "teilt Blätter aus",
+    "Klausurrelevant", "STOP! STOP! STOP!", "Auf Moodle", "Hausaufgaben last-minute", "öööhh, ähh so öäh",
+    "ähh no!", "Did I say that already?", "Ich bin ja kein ...", "Meine Frau ist ja Biologin", "Erwähnt jmd der letzten Reihe",
+    "Regt sich auf"]
+
+
+let middle = "Das Moll'sche Nicken"
+
+
+// let btnCreate = document.getElementById("create");
+// btnCreate.onclick = function(event){
+//     generateTable();
+// }
+let grid = [];
+let size = 5;
+
+let amount = 4;
+
+let content = "";
+generateGrid();
+
+for(let ii = 0; ii < amount; ii++){
+    
+    content += grid[0];
+
+    for(let i = 1; i < size*size; i++){
+        if(i % size == 0)
+            content += ";\n" + grid[i];
+        else
+            content += ";" + grid[i];
+    }
+    content += ";\n\n";
+    grid.length = 0;
+    generateGrid();
+}
+
+// fs.writeFile('bingo.csv', content, err => {
+//     if(err){
+//         console.error(err);
+//         return;
+//     }
+// })
+
+document.addEventListener('click', function(e){
+    e = e || window.event;
+    var target = e.target || e.srcElement,
+    text = target.textContent || target.innerText;
+
+    if(target.className == "cellButton")
+        target.style.color = target.style.color != "red" ? "red" : "black";
+}, false)
+
+function generateTable(){
+    overrideGrid();
+    var table = document.getElementById("bingo");
+    if(table.rows.length > 0){
+        while(table.rows.length != 0){
+            table.deleteRow(0);
+        }
+    }
+    let counter = 0;
+    for(let i = 0; i < size; i++){
+        var row = table.insertRow(i);
+        for(let ii = 0; ii < size; ii++){
+            row.insertCell(ii).innerHTML = `<a class="cellButton">${grid[counter]}</a>`;
+            counter++;
+        }
+    }
+}
+
+function generateGrid(){
+
+    // If current element == center
+
+    // if(grid.length == 12){
+    //     //Math.floor(size*Math.floor(size*.5) + Math.floor(size*.5))){
+    //     grid.push(middle);
+    //     generateGrid();
+    //     return;
+    // }
+    if(grid.length < size*size){
+        let toAdd = fields[getRandomInt(fields.length)];
+        if(!grid.includes(toAdd))
+            grid.push(toAdd);
+        generateGrid();
+    }
+}
+
+function overrideGrid(){
+    let counter = 0;
+    if(grid.length < size*size){
+        let toAdd = fields[getRandomInt(fields.length)];
+        if(!grid.includes(toAdd)){
+            grid[counter] = toAdd;
+            counter++;
+        }
+        overrideGrid();
+        console.log(counter);
+    }
+}
+
+function getRandomInt(max) {
+    // Return number between 0 and (max-1)
+    return Math.floor(Math.random() * max);
+}
