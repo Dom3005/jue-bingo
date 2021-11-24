41ca7b2eaad7954f560210e180224ecd86e0f267
Dom3005 <30704218+Dom3005@users.noreply.github.com>
2021-11-24 20:09:29 +0100

    added new quote

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ let fields =
     "YESSS!", "vielleicht schonmal gehört", "Anekdoten", "wie sagt man..", "teilt Blätter aus",
     "Klausurrelevant", "STOP! STOP! STOP!", "Auf Moodle", "Hausaufgaben last-minute", "öööhh, ähh so öäh",
     "ähh no!", "Did I say that already?", "Ich bin ja kein ...", "Meine Frau ist ja Biologin", "Erwähnt jmd der letzten Reihe",
-    "Regt sich auf"]
+    "Regt sich auf", "Zeigt aufgeregt mit dem Finger auf Leute"]
 
 
 let middle = "Das Moll'sche Nicken"
